8260e2b623540ebf9a928fceb6e49f4e37f0075c
Habibie Faried <habibiefaried@gmail.com>
2020-11-25 17:50:35 +0700

    add cni

--- bootstrap-nomad-client.sh
+++ bootstrap-nomad-client.sh
@@ -111,3 +111,6 @@ WantedBy=multi-user.target
 EOF'
 sudo systemctl enable consul
 sudo systemctl start consul
+curl -L -o cni-plugins.tgz https://github.com/containernetworking/plugins/releases/download/v0.8.7/cni-plugins-linux-amd64-v0.8.7.tgz
+sudo mkdir -p /opt/cni/bin
+sudo tar -C /opt/cni/bin -xzf cni-plugins.tgz
